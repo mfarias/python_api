4f7235d64b4cb1edfc782d73e3ab482d73534af2
mfarias <marcela.sfarias@gmail.com>
2021-05-17 21:34:13 -0300

    Config database

--- app.py
+++ app.py
@@ -5,11 +5,14 @@ import flask.scaffold
 flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
 from flask_restful import Resource, Api, fields, marshal_with, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://pojflcgsicdxcm:253c3360ac00e62aa1cf9354984ec9171ea640b6f68462f035bd83f0fd093a49@ec2-184-73-198-174.compute-1.amazonaws.com:5432/d3l6eje04o82ao'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 class UserModel(db.Model):
     id = db.Column(db.Integer, primary_key=True)
