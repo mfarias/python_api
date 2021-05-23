3bdca429d7203921e27b880e8f94f4a0f9d20de8
mfarias <marcela.sfarias@gmail.com>
2021-05-22 21:00:57 -0300

    Add get user by id endpoint

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask
 # https://github.com/flask-restful/flask-restful/pull/913
 import flask.scaffold
 flask.helpers._endpoint_from_view_func = flask.scaffold._endpoint_from_view_func
-from flask_restful import Resource, Api, fields, marshal_with, request
+from flask_restful import Resource, Api, fields, marshal_with, request, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://pojflcgsicdxcm:253c3360ac00e62aa1cf9354984ec9171ea640b6f68462f035bd83f0fd093a49@ec2-184-73-198-174.compute-1.amazonaws.com:5432/d3l6eje04o82ao'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/user_api'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
@@ -36,13 +36,26 @@ class User(Resource):
         return "Users API"
 
     @marshal_with(resource_fields)
+    def get(self, user_id):
+        result = UserModel.query.filter_by(id=user_id).first()
+        if result:
+            return result, 200
+        else:
+            return abort(404)
+
+api.add_resource(User, '/users/<user_id>')
+
+
+class Users(Resource):
+
+    @marshal_with(resource_fields)
     def get(self):
         page = request.args.get('page', 1, type=int)
         per_page = request.args.get('per_page', 5, type=int)
         result = UserModel.query.paginate(page, per_page, False)
         return result.items
 
-api.add_resource(User, '/users')
+api.add_resource(Users, '/users')
 
 if __name__ == '__main__':
     app.run()
